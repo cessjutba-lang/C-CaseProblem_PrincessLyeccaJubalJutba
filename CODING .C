14da992fd17f1acbb6207f0ab941c93e075b311c
cessjutba-lang <cessjutba@gmail.com>
2025-10-17 07:30:35 -0700

    Add files via upload

---
+++ CODING .C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Display name and ID
+    printf("Name: Princess Lyecca J. Jutba\n");
+    printf("ID Number: 2025303947\n\n");
+
+    // Simple addition
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+
+    // Display the result
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+
+    return 0;
+}
